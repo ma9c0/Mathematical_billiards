959bbc2b9e70319177c45ae0caf70dd7a40c50b6
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2024-11-15 01:40:45 -0500

    .6 version

--- billiards-0.2.6.py
+++ billiards-0.2.6.py
@@ -4,17 +4,15 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse, Circle
 
 a, b = 2, 1
-step = 0.075
 attraction_point = [0.0, 0.0]
 attraction_radius = 0.5
 gravity = 0.0
 
-# List of initial positions and angles
 initial_conditions = [
-    ([0.0, 1.0], np.pi / 4),
-    ([1.0, 0.5], np.pi / 3),
-    ([-1.0, 0.5], np.pi / 6),
-    ([0.5, -1.0], 3 * np.pi / 4)
+    ([0.0, 1.5], np.pi / 3),
+    ([1.5, 0.5], np.pi / 4),
+    ([-1.5, 0.5], np.pi / 6),
+    ([0.5, -1.5], 2 * np.pi / 3)
 ]
 
 fig, (ax_phase, ax_position_angle) = plt.subplots(1, 2, figsize=(15, 5))
@@ -22,11 +20,11 @@ colors = ['b', 'g', 'r', 'm']
 
 ax_phase.set_xlim(-a, a)
 ax_phase.set_ylim(0, 1.0)
-ax_phase.set_xlabel("x-coordinate (at bounce)")
-ax_phase.set_ylabel("tangent velocity (at bounce)")
+ax_phase.set_xlabel("x-coordinate")
+ax_phase.set_ylabel("tangent velocity")
 
 ax_position_angle.set_xlim(-a - 1, 3 * a + 1)
-ax_position_angle.set_ylim(0, np.pi)  # Angle range in radians
+ax_position_angle.set_ylim(0, np.pi) 
 ax_position_angle.set_xlabel("x-coordinate")
 ax_position_angle.set_ylabel("Angle (radians)")
 
@@ -56,6 +54,15 @@ def calculate_tangent_velocity(x, y, vx, vy, a, b):
     tangent_velocity = vx * tangent_x + vy * tangent_y
     return np.abs(tangent_velocity)
 
+def calculate_reflection_position_angle(x, y, vx, vy, a, b):
+    A = (vx ** 2 / a ** 2) + (vy ** 2 / b ** 2)
+    B = 2 * ((x * vx) / a ** 2 + (y * vy) / b ** 2)
+    C = (x ** 2 / a ** 2) + (y ** 2 / b ** 2) - 1
+    t_collision = (-B + np.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+    x_new = x + vx * t_collision
+    y_new = y + vy * t_collision
+    return x_new, y_new, t_collision
+
 for idx, (initial_position, initial_angle) in enumerate(initial_conditions):
     position = initial_position
     velocity = [np.cos(initial_angle), np.sin(initial_angle)]
@@ -66,25 +73,13 @@ for idx, (initial_position, initial_angle) in enumerate(initial_conditions):
     for _ in range(n_reflection):
         x_0, y_0 = position
         vx, vy = velocity
-        x_new = x_0 + vx * step
-        y_new = y_0 + vy * step
 
-        if is_in_field(x_new, y_new):
-            down = [attraction_point[0] - x_new, attraction_point[1] - y_new]
-            magnitude = np.hypot(down[0], down[1])
-            vx += (down[0] / magnitude) * gravity
-            vy += (down[1] / magnitude) * gravity
-            velocity = [vx, vy]
-            x_new = x_0 + vx * step
-            y_new = y_0 + vy * step
-
-        if check_collision(x_new, y_new, a, b):
-            vx, vy = reflect_off_ellipse(x_0, y_0, vx, vy, a, b)
-            x_new = x_0 + vx * step
-            y_new = y_0 + vy * step
+        x_new, y_new, t_collision = calculate_reflection_position_angle(x_0, y_0, vx, vy, a, b)
+        if t_collision > 0:
+            vx, vy = reflect_off_ellipse(x_new, y_new, vx, vy, a, b)
             velocity = [vx, vy]
             tangent_velocity = calculate_tangent_velocity(x_0, y_0, vx, vy, a, b)
-            phase_data.append((x_0, tangent_velocity))
+            phase_data.append((x_new, tangent_velocity))
 
             angle = np.arctan2(vy, vx)
             if y_new > 0:
@@ -92,7 +87,7 @@ for idx, (initial_position, initial_angle) in enumerate(initial_conditions):
             else:
                 position_angle_data.append((x_new, abs(angle)))
 
-        position = [x_new, y_new]
+            position = [x_new, y_new]
 
     if phase_data:
         phase_x, phase_y = zip(*phase_data)
@@ -106,4 +101,3 @@ ax_phase.legend()
 ax_position_angle.legend()
 
 plt.show()
-
